4f7b7ced3d781d0267950c9f0495e4972331b22d
Shivam0Anand <shivam_anand@outlook.com>
2019-05-04 11:27:30 +0530

    can convert celcius to fahrenheit and vice versa

--- app/src/main/java/com/shivam/tempraturechanger/MainActivity.java
+++ app/src/main/java/com/shivam/tempraturechanger/MainActivity.java
@@ -3,13 +3,72 @@ package com.shivam.tempraturechanger;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editCel;
+    private EditText editFar;
+    private TextView cResult;
+    private TextView fResult;
+
+    double c;
+    double f;
+
+   public void tempratureConverter(View view){
+        editCel = (EditText) findViewById(R.id.editCel);
+        editFar = (EditText) findViewById(R.id.editFar);
+        cResult = (TextView) findViewById(R.id.cResult);
+        fResult = (TextView) findViewById(R.id.fResult);
+
+        String c_str = editCel.getText().toString();
+        String f_str = editFar.getText().toString();
+
+        if(c_str.isEmpty()){
+            c=0;
+        }
+        else {
+            c = Double.parseDouble(c_str);
+        }
+
+        if (f_str.isEmpty()){
+            f=0;
+        }
+        else
+            f = Double.parseDouble(f_str);
+
+        double cR, fR;
+
+        //cR = (f - 32) * 5 / 9;
+        //fR = c * 9 / 5 + 32;
+
+        if (!c_str.isEmpty()){
+            fR = c * 9 / 5 + 32;
+            fResult.setText((String.valueOf((double)Math.round(fR*100)/100)+"F"));
+        }
+        else{
+            //cR = (f - 32) * 5 / 9;
+            fResult.setText(" ");
+        }
+        if (!f_str.isEmpty()){
+            cR = (f - 32) * 5 / 9;
+            cResult.setText(String.valueOf((double)Math.round(cR*100)/100) + "C");
+        }
+        else
+            cResult.setText(" ");
+
+        //cResult.setText(String.valueOf(cR)+"C");
+        //fResult.setText(String.valueOf(fR)+"F");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +109,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
